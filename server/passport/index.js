a6949181dd0ca62547ab416377238e4971b0da22
ys <ys.heo.korea@gmail.com>
2022-08-23 18:06:36 +0900

    oauth 2.0

---
+++ server/passport/index.js
@@ -0,0 +1,29 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const passport = require("passport");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+      },
+      function (accessToken, refreshToken, profile, done) {
+        console.log("profile", profile);
+      console.log("accessToken", accessToken);
+      console.log("refreshToken", refreshToken);
+        done(null, profile);
+      }
+    )
+  );
+
+passport.serializeUser((user, done) => {
+done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+done(null, user);
+});
