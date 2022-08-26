324d9c277828192e2f5193311c44436e1b4ce72c
DevUreak <rlaquddlf3204@gmail.com>
2022-08-26 13:43:07 +0900

    recently sync

--- client/src/api/GetBoardList.js
+++ client/src/api/GetBoardList.js
@@ -3,8 +3,10 @@ import * as global from '../Global';
 
 export async function GetBoardList(par_userId) {
     try {
-    
-      const res = await axios.get(global.BASE_URL+"api/post/feed/all/"+"630496a9b27dd713313652eb");
+      //630496a9b27dd713313652eb
+      const res = await axios.post(global.BASE_URL+"api/post/feed/all/",{
+        userId: "630496a9b27dd713313652eb"
+      });
       return res.data;
 
     } catch (error) {
