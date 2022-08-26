e5412bbcb9629a30a68b00a5b9b55066eb1c6513
DevUreak <rlaquddlf3204@gmail.com>
2022-08-26 11:18:52 +0900

    asdasd

--- server/api_doc/README.md
+++
@@ -1,7 +0,0 @@
-# Insomnia API Documenter
-
-## 사용법
-
-1. insomnia-documenter 폴더로 이동
-2. `npx serve` 명령 실행
-
--- server/routes/main.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// const controller = require("../controllers/main.controller");
-
-// router.get("/ss", controller.main_get)
-
-module.exports = router;
